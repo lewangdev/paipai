88a492ab9f0edc6b963dca6147cc59f651c03120
thisiswangle <wangle.tju@gmail.com>
2015-12-07 23:35:18 +0800

    添加入口、主窗口、状态窗口ui

--- src/app.py
+++ src/app.py
@@ -1,14 +1,24 @@
 #coding=utf-8
 
-from gui import main_panel as mp
+from gui import main_panel
 import wx
 
 class MainFrame(wx.Frame):
     def __init__(self, parent, title):
-        wx.Frame.__init__(self, parent, title=title, size=(428,448), style= wx.MINIMIZE_BOX | wx.SYSTEM_MENU | wx.CAPTION | wx.CLOSE_BOX)
-        main_panel = mp.MainPanel(self)
+        wx.Frame.__init__(self, parent, title=title, size=(428,448), style=wx.MINIMIZE_BOX | wx.SYSTEM_MENU | wx.CAPTION | wx.CLOSE_BOX)
+        self.m_main_panel = main_panel.MainPanel(self)
         self.Show(True)
 
-app = wx.App(False)
-main_frame = MainFrame(None, u"拍牌设置")
-app.MainLoop()
+class App(wx.App):
+    def OnInit(self):
+        self.m_main_frame = MainFrame(None, u'测试')
+        self.SetTopWindow(self.m_main_frame)
+        self.m_main_frame.Show()
+        return True
+
+def main():
+    app = App()
+    app.MainLoop()
+
+if __name__ == '__main__':
+    main()
