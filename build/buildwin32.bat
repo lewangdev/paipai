66d2584382b36e9ee9291862ae76908268cf0052
thisiswangle <wangle.tju@gmail.com>
2015-12-08 11:00:18 +0800

    update build script

--- build/buildwin32.bat
+++ build/buildwin32.bat
@@ -1,1 +1,8 @@
-pyinstaller ../src/app.py -F --distpath ../dist -p %PYTHONPATH%/Lib/site-packages/pil;
+pyinstaller ../src/app.py ^
+    --noconsole^
+    --onefile ^
+    --distpath=../dist ^
+    --key=we3FeKcRVUun7Gul ^
+    --upx-dir=C:\upx391w\upx391w ^
+    --icon=../src/paipai.ico ^
+    -p %PYTHONPATH%/Lib/site-packages/pil;
