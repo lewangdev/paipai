a7bdf7674cb95121e9d938e63232f1bbb497e6d7
thisiswangle <wangle.tju@gmail.com>
2015-12-09 12:12:04 +0800

    添加方法将图片二值化

--- src/utils/im.py
+++ src/utils/im.py
@@ -59,6 +59,30 @@ def capture_window_rect(hwnd, rect):
     return im_window.crop()
 
 
+def binarization(im, threshold=None, inverse=False):
+    '''对图片进行二值化操作
+    '''
+    # 灰度处理
+    im = im.convert('L')
+
+    w, h = im.size
+
+    if threshold is None:
+        threshold = 0
+        for x in xrange(w):
+            for y in xrange(h):
+                threshold += im.getpixel(x, y)
+
+        threshold /= w * h
+
+    for x in xrange(w):
+        for y in xrange(h):
+            if (im.getpixel((x, y)) > threshold):
+                im.putpixel((x, y), 0xFF if not inverse else 0x00)
+            else:
+                im.putpixel((x, y), 0x00 if not inverse else 0xFF)
+    return im
+
 if __name__ == '__main__':
     hwnd = win32gui.GetForegroundWindow()
     im = capture_window(hwnd)
