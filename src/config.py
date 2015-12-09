48c4b24dc63e930049ca6a7cf7accfcf8d7a8d64
thisiswangle <wangle.tju@gmail.com>
2015-12-09 23:34:25 +0800

    mv config.py out

---
+++ src/config.py
@@ -0,0 +1,19 @@
+#coding=utf8
+
+button_pos = {
+        "raise":(),
+        "bid":(),
+        "bid_confirm":(),
+        "bid_cancel":()}
+
+input_pos = {
+        "custom_delta":(),
+        "custom_price":(),
+        "check_code":()}
+
+capture_pos = dict(
+        plate_limit=(131, 273),
+        participant_count=(159, 288),
+        system_time=(145, 385),
+        price_accepted=(173, 401),
+        time_price_accepted=(201, 417))
