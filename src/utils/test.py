ca5262954f437275d92b241b11dc06b15412118c
thisiswangle <wangle.tju@gmail.com>
2015-12-23 00:06:46 +0800

    添加查看窗口工具

---
+++ src/utils/test.py
@@ -0,0 +1,21 @@
+#coding=utf8
+
+import im
+import browser
+import window
+import time
+
+if __name__ == '__main__':
+    ie = browser.open_ie(url="http://moni.51hupai.org")
+    #hwnd = window.find_sub_hwnd(ie.HWND, [("Frame Tab", 0),
+    #    ("TabWindowClass", 0),
+    #    ("Shell DocObject View", 0),
+    #    ("Internet Explorer_Server", 0)])
+    hwnd = window.find_sub_hwnd(ie.HWND, [("Frame Tab", 0)])
+    print "%x" % (hwnd)
+
+    for i in xrange(100):
+        im.capture_window(hwnd)
+        im.save('test_%s.bmp' % i, 'BMP')
+        time.sleep(1)
+
---
+++ src/utils/window.py
@@ -0,0 +1,40 @@
+#coding=utf8
+"""
+查找窗口句柄.
+
+一个进程窗口由多个窗口组成，需要查找到最直接的那个窗口
+"""
+import win32gui
+
+def find_sub_hwnd_in_index(hwnd, win_class, index=0):
+    """
+    已知子窗口的窗体类名
+    寻找第index号个同类型的兄弟窗口
+    """
+    assert type(index) == int and index >= 0
+    sub_hwnd= win32gui.FindWindowEx(hwnd, 0, win_class, None)
+    while index > 0:
+        sub_hwnd = win32gui.FindWindowEx(hwnd, handle, win_class, None)
+        index -= 1
+    return sub_hwnd
+
+
+def find_sub_hwnd(hwnd, win_classes):
+    """
+    递归寻找子窗口的句柄
+
+    hwnd是祖父窗口的句柄
+    win_classes是各个子窗口的class列表，父辈的list-index小于子辈
+    """
+    assert type(win_classes) == list
+    if len(win_classes) == 1:
+        return find_sub_hwnd_in_index(hwnd, win_classes[0][0], win_classes[0][1])
+    else:
+        hwnd = find_sub_hwnd_in_index(hwnd, win_classes[0][0], win_classes[0][1])
+        return find_sub_hwnd(hwnd, win_classes[1:])
+
+
+if __name__ == '__main__':
+    ie_hwnd = 0x000c037c
+    hwnd = find_sub_hwnd(ie_hwnd, [("Frame Tab", 0), ("TabWindowClass", 0), ("Shell DocObject View", 0), ("Internet Explorer_Server", 0)])
+    print "%x" % (hwnd)
